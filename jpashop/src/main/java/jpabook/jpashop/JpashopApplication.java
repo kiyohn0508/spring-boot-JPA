e00f6e6a695bad7f6cb70c5eb683cfcc14243ead
kiyohn0508 <kiyohn0508@gmail.com>
2025-05-26 14:31:47 +0000

    resetting up

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JpashopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JpashopApplication.class, args);
 	}
 
 }
--- jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class JpashopApplicationTests {
 
 	@Test
 	void contextLoads() {
